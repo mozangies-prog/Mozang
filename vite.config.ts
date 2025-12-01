d6815493738e0fed792bbf34808f52380d4d0e4e
mozangies-prog <mozangies@gmail.com>
2025-12-01 17:49:28 +0500

    Update Vite config to increase chunk size warning limit

--- vite.config.ts
+++ vite.config.ts
@@ -21,3 +21,13 @@ export default defineConfig(({ mode }) => {
       }
     };
 });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+
+  build: {
+    chunkSizeWarningLimit: 1500 // increase limit (in KB)
+  }
+})
